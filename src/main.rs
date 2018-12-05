bd6be68f0cc8c9e5f311c632e79bbbd2be175428
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-12-04 20:33:36 -0800

    Read an input image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use image;
+use image::GenericImageView;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
 fn main() {
-    println!("Hello, world!");
+    let options: CliOptions = CliOptions::from_args();
+    println!("{:?}", options);
+
+    let img = image::open(options.source_image)
+        .expect("open source_image")
+        .resize(400, 300, image::FilterType::Gaussian)
+        .grayscale();
+    println!("{:?}", img.dimensions());
+}
+
+#[derive(StructOpt, Debug)]
+struct CliOptions {
+    #[structopt(name = "source_image", parse(from_os_str))]
+    source_image: PathBuf,
 }
